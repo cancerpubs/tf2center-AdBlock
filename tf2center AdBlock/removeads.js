c4a76da9b949264af6c2a3d4804663c01ff5e349
nobelharvards <nobelharvards@users.noreply.github.com>
2017-06-05 13:38:06 +1000

    Spelling mistakes
    "This is a notification from the TF2center AdBlock extention" to "This is a notification from the TF2Center AdBlock extension".

--- tf2center AdBlock/removeads.js
+++ tf2center AdBlock/removeads.js
@@ -26,7 +26,7 @@ function removeads()
 function addui(){
     var ui = document.getElementsByClassName('freemiumContainer');
         for(var i = 0; i < ui.length; i++) { 
-          ui[i].innerHTML = ui[i].innerHTML + '<div class="ym-grid" style="border: 1px solid #208193; padding: 25px;"><h2>AdBlock</h2><div class="freemiumProgress"><div class="meter ym-g1220-12 ym-gl omega"><div class="progress" style="width: 100%; border-color: #5c7a5a; background-color: #5c7a5a; transition: width 1s ease;"></div></div></div><div class="freemiumStatus"><div>100%</div><div><span>ACTIVATED</span></div></div><img style="width: 120px; position: absolute; margin-left: 12px; margin-top: -25px;" src="/assets/images/achievements/dr_doom_1.png"></div><p style="color: #208193">This is a notification from the TF2center AdBlock extention</p>';
+          ui[i].innerHTML = ui[i].innerHTML + '<div class="ym-grid" style="border: 1px solid #208193; padding: 25px;"><h2>AdBlock</h2><div class="freemiumProgress"><div class="meter ym-g1220-12 ym-gl omega"><div class="progress" style="width: 100%; border-color: #5c7a5a; background-color: #5c7a5a; transition: width 1s ease;"></div></div></div><div class="freemiumStatus"><div>100%</div><div><span>ACTIVATED</span></div></div><img style="width: 120px; position: absolute; margin-left: 12px; margin-top: -25px;" src="/assets/images/achievements/dr_doom_1.png"></div><p style="color: #208193">This is a notification from the TF2Center AdBlock extension</p>';
         }
 }
 
